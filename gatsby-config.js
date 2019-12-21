abc41022fa39271a6d4ee98c09d9d88326d23c86
mojojojo20 <menonk484@gmail.com>
2019-12-21 20:11:53 +0530

    remove lock

--- gatsby-config.js
+++ gatsby-config.js
@@ -44,13 +44,8 @@ module.exports = {
         start_url: "https://karthikmenon.me/",
         background_color: "#1f2b3b",
         theme_color: "#1f2b3b",
-        // Enables "Add to Homescreen" prompt and disables browser UI (including back button)
-        // see https://developers.google.com/web/fundamentals/web-app-manifest/#display
         display: "standalone",
-        icon: "src/images/icon.png", // This path is relative to the root of the site.
-        // An optional attribute which provides support for CORS check.
-        // If you do not provide a crossOrigin option, it will skip CORS for manifest.
-        // Any invalid keyword or empty string defaults to `anonymous`
+        icon: "src/images/icon.png", 
         crossOrigin: `use-credentials`,
       },
     },
@@ -65,7 +60,7 @@ module.exports = {
     
     // this (optional) plugin enables Progressive Web App + Offline functionality
     // To learn more, visit: https://gatsby.dev/offline
-    // `gatsby-plugin-offline`,
+    `gatsby-plugin-offline`,
     
   ],
 }
