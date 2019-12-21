7ae8311d221b0d57f5d1db7a8c7ed49e86d3283f
mojojojo20 <menonk484@gmail.com>
2019-12-21 20:02:37 +0530

    change

--- gatsby-config.js
+++ gatsby-config.js
@@ -47,7 +47,7 @@ module.exports = {
         // Enables "Add to Homescreen" prompt and disables browser UI (including back button)
         // see https://developers.google.com/web/fundamentals/web-app-manifest/#display
         display: "standalone",
-        icon: "/logo.png", // This path is relative to the root of the site.
+        icon: "src/images/icon.png", // This path is relative to the root of the site.
         // An optional attribute which provides support for CORS check.
         // If you do not provide a crossOrigin option, it will skip CORS for manifest.
         // Any invalid keyword or empty string defaults to `anonymous`
