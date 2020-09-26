b7bac210fce56e6a4763f0b47c7c9cb08ff0a2b3
mojojojo20 <menonk484@gmail.com>
2020-09-26 16:35:13 +0530

    refactor exCurricular

--- src/components/ExtraCurricular.js
+++ src/components/ExtraCurricular.js
@@ -1,38 +1,28 @@
 import React from "react";
+import { graphql, useStaticQuery } from "gatsby";
 
 export default function ExtraCurricular() {
+  const query = useStaticQuery(graphql`
+    {
+      allExcurricularYaml {
+        edges {
+          node {
+            point
+          }
+        }
+      }
+    }
+  `);
+  const exCurricularPoints = query.allExcurricularYaml.edges;
   return (
     <>
       <div className="h3 code mx-3 mt-2 mb-3">
         {'<div id="Extra-Curricular-And-Honors">'}
       </div>
       <div className="text-muted mx-5 my-4 h5 text-justify info">
-        <p>Finalist (top 15 teams) at CSI InApp Awards 2020</p>
-        <p>Special Jury Mention at ICTAK Techathlon 2020 (Hackathon)</p>
-        <p>Top 75 teams at MinMash Hacakthon 2020</p>
-        <p>Awarded Dean's Award for best academic performance 2018-2019.</p>
-        <p>
-          Cleared three rounds of TCS Engi-NX 2019, a hackathon by TCS to reach
-          top 150 teams as of 13-06-2019
-        </p>
-        <p>Bagged first prize in Ad-On Event, Bharatham 2018</p>
-        <p>Active participant in Sports (Football and Athletics)</p>
-        <p>
-          Bagged first prize in DesignIT 2017,2018. Best UI/UX design, hackIT
-          2018
-        </p>
-        <p>Volunteer at ICFOSS 2018, RSET.</p>
-        <p>
-          Designed promo video for Saavishkar 2018, all Kerala open student
-          exhibition for school students
-        </p>
-        <p>
-          Active photographer and featured in Times Of India, Vadodara edition
-        </p>
-        <p>
-          Won the Dada Saheb Phalke National Award for best PSA (Public Service
-          Ad) in school category
-        </p>
+        {exCurricularPoints.map(({ node }) => {
+          return <p key={node.point.replace(" ", "-")}>{node.point}</p>;
+        })}
       </div>
       <div className="h3 mx-3 code mt-2 mb-3">{"</div>"}</div>
     </>
