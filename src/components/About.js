773f678018f07dd05bf54bc3eb6a5f8c901044c1
mojojojo20 <menonk484@gmail.com>
2020-09-27 19:10:31 +0530

    fix typos

--- src/components/About.js
+++ src/components/About.js
@@ -5,13 +5,14 @@ export default function About() {
     <>
       <div className="h3 code mx-3 mt-2 mb-3">{'<div id="About">'}</div>
       <div className="text-muted mx-5 my-4 h4 text-justify info">
-        A software engineer working at BigBinary. Passionate about making clean,
-        minimalistic designs. Hit me up for any project enquiries. <br />
+        A Software Engineer working remotely at BigBinary. Passionate about
+        making clean, minimalistic designs. Hit me up for any project enquiries.{" "}
+        <br />
         Through my B.tech, I have done a few Full Stack Projects. I have also
         had a my share of interest in Blockchain and IoT. Check out my projects
         on&nbsp;
         <a
-          href="https://github.com/mojojojo20"
+          href="https://github.com/karthiknmenon"
           target="_blank"
           rel="noreferrer"
         >
@@ -20,7 +21,7 @@ export default function About() {
         Always free to discuss website designs, email templating and a lot more
         over <span className="highlight">coffee</span>. <br />
         Looking to freelance your project ?{" "}
-        <a href="mailto:menonk484@gmail.com">Hit Me Up</a>
+        <a href="mailto:menonk484@gmail.com">Hit Me Up.</a>
       </div>
       <div className="h3 mx-3 code mt-2 mb-3">{"</div>"}</div>
     </>
